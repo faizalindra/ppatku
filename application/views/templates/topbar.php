75fb3b285cf87d8cfe5b6c84eca62efd5447a28d
faizalindra <faizalindra9@gmail.com>
2021-11-22 18:28:02 +0700

    fix topbar

--- application/views/templates/topbar.php
+++ application/views/templates/topbar.php
@@ -23,8 +23,8 @@
                     <!-- Nav Item - User Information -->
                 <li class="nav-item dropdown no-arrow">
                     <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
-                        <img class="img-profile rounded-circle" src="<?php base_url('assets/img/undraw_profile_photo.svg');?>">
+                        <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo "Halo, <b>".$this->session->userdata('username');?></span>
+                        <img class="img-profile rounded-circle" src="<?php base_url('assets\img\profile\default.jpg');?>">
                     </a>
                     <!-- Dropdown - User Information -->
                     <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
@@ -32,17 +32,9 @@
                             <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                             Profile
                         </a>
-                        <a class="dropdown-item" href="#">
-                            <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                            Settings
-                        </a>
-                        <a class="dropdown-item" href="">
-                            <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                            Activity Log
-                        </a>
                         <div class="dropdown-divider"></div>
                         <a class="dropdown-item" href="<?= base_url('autentifikasi/logout') ?>">
-                            <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
+                            <i class="fas glyphicon-log-out fa-sm fa-fw mr-2 text-gray-400"></i>
                             Logout
                         </a>
                     </div>
