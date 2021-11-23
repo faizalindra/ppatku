cf28c9dcecb900cad20695d0ba64699680d84600
faizalindra <faizalindra9@gmail.com>
2021-11-23 15:19:32 +0700

    refine tabelBerkas

--- application/views/sidebar/berkas/tabelBerkas.php
+++ application/views/sidebar/berkas/tabelBerkas.php
@@ -148,19 +148,21 @@
                 <!-- <?= $this->session->flashdata('pesan'); ?> -->
                 <table class="table table-hover">
                     <thead>
-                        <tr>
+                        <tr class="text-center">
                             <th scope="col">#</th>
                             <th scope="col">Tanggal Masuk</th>
                             <th scope="col">Reg Sertipikat</th>
                             <th scope="col">Desa</th>
                             <th scope="col">Kecamatan</th>
                             <th scope="col">Jenis Berkas</th>
+                            <th scope="col">Id Proses</th>
                             <th scope="col">Status Proses</th>
                             <th scope="col">Nama Penjual</th>
                             <th scope="col">Nama Pembeli</th>
                             <th scope="col">Biaya</th>
                             <th scope="col">DP</th>
-                            <th scope="col">Keterangan</th>
+                            <th scope="col">Total Biaya</th>
+                            <!-- <th scope="col">Keterangan</th> -->
                             <th scope="col">Berkas Selesai</th>
                         </tr>
                     </thead>
@@ -169,27 +171,46 @@
                         <?php
                         $a = 1;
                         foreach ($berkas as $b) { ?>
-                            <tr>
+                            <tr class="text-capitalize text-center">
                                 <th scope="row"><?= $a++; ?></th>
                                 <td><?= $b['tgl_masuk']; ?></td>
                                 <td><?php
                                     if ($b['reg_sertipikat'] == 0) {
                                         echo " ";
                                     } else {
-                                        $b['reg_sertipikat'];
+                                        echo $b['reg_sertipikat'];
                                     } ?></td>
                                 <td><?= $b['desa']; ?></td>
                                 <td><?= $b['kecamatan']; ?></td>
                                 <td><?= $b['jenis_berkas']; ?></td>
                                 <td><?= $b['id_proses']; ?></td>
                                 <td><?= $b['status_proses']; ?></td>
-                                <td><?= $b['nama_penjual']; ?></td>
-                                <td><?= $b['nama_pembeli']; ?></td>
-                                <td><?= $b['biaya']; ?></td>
-                                <td><?= $b['dp']; ?></td>
-                                <td><?= $b['tot_biaya']; ?></td>
-                                <td><?= $b['keterangan']; ?></td>
-                                <td><?= $b['berkas_selesai']; ?></td>
+                                <td class="text-left"><?= $b['nama_penjual']; ?></td>
+                                <td class="text-left"><?= $b['nama_pembeli']; ?></td>
+                                <td><?php
+                                    if ($b['biaya'] == 0) {
+                                        echo " ";
+                                    } else {
+                                        echo "Rp.", number_format($b['biaya']);
+                                    } ?></td>
+                                <td><?php
+                                    if ($b['dp'] == 0) {
+                                        echo " ";
+                                    } else {
+                                        echo "Rp.", number_format($b['dp']);
+                                    } ?></td>
+                                <td><?php
+                                    if ($b['tot_biaya'] == 0) {
+                                        echo " ";
+                                    } else {
+                                        echo "Rp.", number_format($b['tot_biaya']);
+                                    }?></td>
+                                <td><?php if ($b['berkas_selesai'] == 1) { ?>
+                                        <a href="##" class="badge badge-info"><i class="fas fa-edit"></i> Selesai</a>
+                                    <?php } else { ?>
+                                        <a href="##" class="badge badge-danger"><i class="fas fa-edit"></i>Proses</a>
+                                    <?php }; ?>
+                                </td>
                                 <td>
                                     <!-- <a href="<?= base_url('buku/ubahBuku/') . $b['id']; ?>" class="badge badge-info"><i class="fas fa-edit"></i> Ubah</a> -->
                                     <!-- <a href="<?= base_url('user/hapusUser/') . $b['id']; ?>" onclick="return confirm('Kamu yakin akan menghapus <?= $b['username']; ?> ?');" class="badge badge-danger"><i class="fas fa-trash"></i> Hapus</a> -->
