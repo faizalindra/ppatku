fcbfd8c07d2c9c0a6e9b0fa4e03d9784b88f69cb
faizalindra <faizalindra9@gmail.com>
2021-12-08 21:09:40 +0700

    Tabel Berkas

--- application/controllers/Sertipikat.php
+++ application/controllers/Sertipikat.php
@@ -10,14 +10,35 @@ class Sertipikat extends CI_Controller
         $this->load->model('ModelSertipikat');
     }
 
-    public function uji(){
-        $id=$this->input->get('id');
-        $data=$this->ModelSertipikat->get_sertipikat($id);
+    public function index()
+    {
+
+        $data['sertipikat'] = $this->ModelSertipikat->cekSertipikat()->result_array();
+        $data['judul'] = "Daftar Sertipikat";
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebarAdmin');
+        $this->load->view('templates/topbar');
+        $this->load->view('sidebar/sertipikat/tabelSertipikat', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function uji()
+    {
+        $id = $this->input->get('id');
+        $data = $this->ModelSertipikat->get_sertipikat($id);
         echo json_encode($data);
     }
 
-    public function uji1(){
-        $data=$this->ModelSertipikat->cekSertipikat()->result_array();
+    public function uji1()
+    {
+        $data = $this->ModelSertipikat->cekSertipikat()->result_array();
+        echo json_encode($data);
+    }
+
+
+    function data_sertipikat()
+    {
+        $data = $this->ModelSertipikat->sertipikat_list();
         echo json_encode($data);
     }
 }
